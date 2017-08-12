9910a02baabc005cbe6b01826a3e4118c85c04d8
Unknown <dormeur12h@hotmail.com>
2017-08-12 11:07:25 +0200

    fix responsive

---
+++ src/Lew/MovieBundle/Form/TriType.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lew
+ * Date: 04/08/2017
+ * Time: 21:18
+ */
+
+namespace Lew\MovieBundle\Form;
+
+
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class TriType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('search', TextType::class, array(
+                'attr' => array(
+                    'placeholder' => 'Rechercher',
+                ),
+                'required' => false,
+            ))
+            ->add('genre', EntityType::class, array(
+                'class' => 'Lew\ApiBundle\Entity\Genre',
+                'choice_label' => 'name',
+                'placeholder' => '-- Genre --',
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('g')
+                        ->orderBy('g.name', 'ASC');
+                    },
+                'required' => false,
+                )
+            )
+            ->add('ordre', ChoiceType::class, array(
+                'choices' => array(
+                    'Titre' => 'title',
+                    'Note' => 'note',
+                    'Date' => 'dateSortie',
+                    'Durée' => 'duree',
+                    ),
+                'data' => 'title'
+            ))
+            ->add('classement', ChoiceType::class, array(
+                'choices' => array(
+                    'Croissant' => 'ASC',
+                    'Décroissant' => 'DESC',
+                ),
+                'data' => 'ASC',
+            ))
+            ->add('submit', SubmitType::class, array(
+                'label' => 'Trier',
+                'attr' => array(
+                    'class' => 'btn btn-info',
+                )
+            ));
+    }
+}
